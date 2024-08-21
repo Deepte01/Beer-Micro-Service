d7c1db3e3a4283612da2fbd77b845318fdfadca9
Deepika Teegapuram <dteegapuram@gmail.com>
2024-08-20 19:33:28 -0600

    Initial Commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/beermicroservice/BeerMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.app.ws.beermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerMicroServiceApplication.class, args);
+    }
+
+}
